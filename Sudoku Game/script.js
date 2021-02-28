5920c6b2f46613aa06f56407333fc29fd252ab42
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-28 23:28:30 +0530

    Update script.js

--- Sudoku Game/script.js
+++ Sudoku Game/script.js
@@ -1,29 +1,27 @@
 var arr = [[], [], [], [], [], [], [], [], []]
 var temp = [[], [], [], [], [], [], [], [], []]
 
-for (var i = 0; i < 9; i++) {
-    for (var j = 0; j < 9; j++) {
+for (var i = 0; i < 9; i++) 
+{   for (var j = 0; j < 9; j++) 
+    {   
         arr[i][j] = document.getElementById(i * 9 + j);
-
     }
 }
 
-function initializeTemp(temp) {
-
-    for (var i = 0; i < 9; i++) {
-        for (var j = 0; j < 9; j++) {
+function initializeTemp(temp) 
+{   for (var i = 0; i < 9; i++) 
+    {   for (var j = 0; j < 9; j++) 
+        {
             temp[i][j] = false;
-
         }
     }
 }
 
-
-function setTemp(board, temp) {
-
-    for (var i = 0; i < 9; i++) {
-        for (var j = 0; j < 9; j++) {
-            if (board[i][j] != 0) {
+function setTemp(board, temp) 
+{   for (var i = 0; i < 9; i++) 
+    {   for (var j = 0; j < 9; j++) 
+        {   if (board[i][j] != 0) 
+            {
                 temp[i][j] = true;
             }
 
@@ -31,27 +29,22 @@ function setTemp(board, temp) {
     }
 }
 
-
-function setColor(temp) {
-
-    for (var i = 0; i < 9; i++) {
-        for (var j = 0; j < 9; j++) {
-            if (temp[i][j] == true) {
+function setColor(temp) 
+{   for (var i = 0; i < 9; i++) 
+    {   for (var j = 0; j < 9; j++) 
+        {   if (temp[i][j] == true) 
+            {
                 arr[i][j].style.color = "#DC3545";
             }
-
         }
     }
 }
 
-function resetColor() {
-
-    for (var i = 0; i < 9; i++) {
-        for (var j = 0; j < 9; j++) {
-
+function resetColor() 
+{   for (var i = 0; i < 9; i++) 
+    {   for (var j = 0; j < 9; j++) 
+        {
             arr[i][j].style.color = "green";
-
-
         }
     }
 }
