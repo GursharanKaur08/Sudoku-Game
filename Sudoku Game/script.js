8e8cf97b66fdebcf9a75592e74df3af1610ee619
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-28 23:23:51 +0530

    Update script.js

---
+++ Sudoku Game/script.js
@@ -0,0 +1,9 @@
+var arr = [[], [], [], [], [], [], [], [], []]
+var temp = [[], [], [], [], [], [], [], [], []]
+
+for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+        arr[i][j] = document.getElementById(i * 9 + j);
+
+    }
+}
