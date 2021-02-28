d3b51b48e2f637aaab1f55d6d1b3d89ced803709
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-28 23:25:04 +0530

    Update script.js

--- Sudoku Game/script.js
+++ Sudoku Game/script.js
@@ -7,3 +7,51 @@ for (var i = 0; i < 9; i++) {
 
     }
 }
+
+function initializeTemp(temp) {
+
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            temp[i][j] = false;
+
+        }
+    }
+}
+
+
+function setTemp(board, temp) {
+
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (board[i][j] != 0) {
+                temp[i][j] = true;
+            }
+
+        }
+    }
+}
+
+
+function setColor(temp) {
+
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (temp[i][j] == true) {
+                arr[i][j].style.color = "#DC3545";
+            }
+
+        }
+    }
+}
+
+function resetColor() {
+
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+
+            arr[i][j].style.color = "green";
+
+
+        }
+    }
+}
