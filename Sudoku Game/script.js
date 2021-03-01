c40ebb073f4671bc5124494952f2339bb719c135
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-01 22:26:51 +0530

    Update script.js

--- Sudoku Game/script.js
+++ Sudoku Game/script.js
@@ -113,7 +113,30 @@ function isPossible(board, sr, sc, val)
         }
     }
     return true;
+}
 
+function solveSudokuHelper(board, sr, sc) {
+    if (sr == 9) {
+        changeBoard(board);
+        return;
+    }
+    if (sc == 9) {
+        solveSudokuHelper(board, sr + 1, 0)
+        return;
+    }
+
+    if (board[sr][sc] != 0) {
+        solveSudokuHelper(board, sr, sc + 1);
+        return;
+    }
+
+    for (var i = 1; i <= 9; i++) {
+        if (isPossible(board, sr, sc, i)) {
+            board[sr][sc] = i;
+            solveSudokuHelper(board, sr, sc + 1);
+            board[sr][sc] = 0;
+        }
+    }
 }
 
 
